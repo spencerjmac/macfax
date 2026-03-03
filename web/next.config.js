85ce8dadf13be067b22585d0aa5e6b1ca3fd3232
Jay McEntire <599790+jmacul2@users.noreply.github.com>
2026-03-03 14:29:40 -0700

    more updates to build

--- web/next.config.js
+++ web/next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 
+const srcDir = path.resolve(__dirname, 'src')
+
 const nextConfig = {
   images: {
     unoptimized: true,
   },
   basePath: process.env.NODE_ENV === 'production' ? '' : '',
   webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname, 'src')
+    config.resolve.alias = config.resolve.alias || {}
+    config.resolve.alias['@'] = srcDir
+    config.resolve.alias['@/'] = srcDir + path.sep
     return config
   },
 }
