f1d24a90742aa216afa88cea5b7b879952edd729
Jay McEntire <599790+jmacul2@users.noreply.github.com>
2026-03-03 14:26:44 -0700

    updates to docker posts and changes on react comile for docker

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   images: {
     unoptimized: true,
   },
   basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src')
+    return config
+  },
 }
 
 module.exports = nextConfig
